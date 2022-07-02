b9c94b2f50fb8447b45d88c4f34198024fdee326
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 17:19:49 -0300

    aula 1 - config e primeiro teste

---
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from '@testing-library/react-native';
+import {StarRating} from '../StarRating';
+
+describe('StarRating', () => {
+  it('the component rendered', () => {
+    const {debug} = render(<StarRating rating={{average: 5}} />);
+
+    debug('StarRating');
+  });
+});
