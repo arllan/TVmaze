3ee700079d37f41b00ba3d75da54f3bf851b7a0e
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-02 19:29:23 -0300

    aula 2 - queries e variants

--- src/components/StarRating/__tests__/StarRating.test.tsx
+++ src/components/StarRating/__tests__/StarRating.test.tsx
@@ -3,9 +3,17 @@ import {render} from '@testing-library/react-native';
 import {StarRating} from '../StarRating';
 
 describe('StarRating', () => {
-  it('the component rendered', () => {
-    const {debug} = render(<StarRating rating={{average: 5}} />);
+  test('if passed rating, show value', () => {
+    const {getByText, getByTestId} = render(
+      <StarRating rating={{average: 5}} />,
+    );
+    expect(getByText('5')).toBeTruthy();
+    expect(getByTestId('starIcon')).toBeTruthy();
+  });
+  test('if there is not rating show nothing ', () => {
+    const {container, queryByTestId} = render(<StarRating />);
 
-    debug('StarRating');
+    expect(queryByTestId('starIcon')).toBeNull();
+    expect(container.children).toEqual([]);
   });
 });
