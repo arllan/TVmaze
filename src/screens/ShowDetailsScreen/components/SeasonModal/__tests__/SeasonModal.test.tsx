9a672842ab23874b6788753eb6cdfd24fced984f
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-05 12:46:47 -0300

    aula 3 - Season full test

--- src/screens/ShowDetailsScreen/components/SeasonModal/__tests__/SeasonModal.test.tsx
+++ src/screens/ShowDetailsScreen/components/SeasonModal/__tests__/SeasonModal.test.tsx
@@ -1,12 +1,47 @@
-import React from 'react';
+import {render, act, fireEvent} from '@testing-library/react-native';
+import React, {createRef} from 'react';
+import {Modalize} from 'react-native-modalize';
 import {SeasonModal} from '../SeasonModal';
 
 describe('SeasonModal', () => {
-  test('the component rendered', () => {
-    <SeasonModal
-      seasons={['1', '2', '3']}
-      selectedSeason="1"
-      onSelectSeason={season => console.log(season)}
-    />;
+  test('show all season options', async () => {
+    const modalizeRef = createRef<Modalize>();
+    const {getAllByText} = render(
+      <SeasonModal
+        ref={modalizeRef}
+        selectedSeason="1"
+        seasons={['1', '2', '3']}
+        onSelectSeason={season => console.log(season)}
+      />,
+    );
+
+    act(() => {
+      modalizeRef.current?.open();
+    });
+
+    expect(getAllByText(/season/i, {exact: false}).length).toBe(3);
+  });
+  test('call onSelectSeason with correct season when press season', async () => {
+    const onSelectSeasonMock = jest.fn();
+
+    const modalizeRef = createRef<Modalize>();
+    const {getByText} = render(
+      <SeasonModal
+        ref={modalizeRef}
+        selectedSeason="1"
+        seasons={['1', '2', '3']}
+        onSelectSeason={onSelectSeasonMock}
+      />,
+    );
+
+    act(() => {
+      modalizeRef.current?.open();
+    });
+
+    const season3Element = getByText(/3/);
+
+    fireEvent.press(season3Element);
+
+    expect(onSelectSeasonMock).toBeCalledWith('3');
   });
 });
