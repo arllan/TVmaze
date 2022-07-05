e0269e80fbaf85db302f7562f750ff388e7aa3e3
Lucas Garcez <lucasgarcezdev@gmail.com>
2022-07-05 20:41:02 -0300

    aula - handle libs

--- src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
+++ src/screens/ShowDetailsScreen/components/EpisodeList/__tests__/EpisodeList.test.tsx
@@ -3,8 +3,32 @@ import {render} from '@testing-library/react-native';
 import {EpisodeList} from '../EpisodeList';
 
 import {mocks} from './mocks';
+import {QueryClient, QueryClientProvider, setLogger} from 'react-query';
+import {showService} from '../../../../../services/show/showService';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: Infinity,
+      retry: false,
+    },
+  },
+});
+
+const wrapper = ({children}) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
 describe('EpisodeList', () => {
   test('first render show all season one episodes', () => {
-    render(<EpisodeList show={mocks.show} />);
+    jest.spyOn(showService, 'getEpisodes').mockResolvedValue({
+      seasons: {
+        1: [],
+      },
+      seasonNames: [],
+    });
+    render(<EpisodeList show={mocks.show} />, {wrapper});
+
+    expect(true).toBeTruthy();
   });
 });
